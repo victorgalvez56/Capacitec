542a21665e5dc07ee38ef1a62d7b679983ac7fa0
victorgalvezsma <victor.galvez@smasoftware.com>
2023-10-13 17:08:08 -0500

    test webconfig

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    resolve: {
+        alias: {
+            "react-native$": "react-native-web",
+            "normalize-css-color": "@react-native/normalize-color",
+        }
+    },
+};
