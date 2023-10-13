141f819679a63f76d8c0d488c5ba44f06c6bb55a
victorgalvezsma <victor.galvez@smasoftware.com>
2023-10-13 17:11:59 -0500

    remove webconfig

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    resolve: {
-        alias: {
-            "react-native$": "react-native-web",
-            "normalize-css-color": "@react-native/normalize-color",
-        }
-    },
-};
